17fa6071063622b730f7d9dce2796ca894168017
gusttazy <yguusta22amador@gmail.com>
2025-06-04 16:18:04 -0400

    Configurações de rotas de usuarios autenticados; configuração de layout para as telas do app; reestruturação e organização de código.

---
+++ components/Header/index.tsx
@@ -0,0 +1,40 @@
+import { Clover, UsersRound } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export default function Header() {
+    return (
+        <header className="border-b">
+            <div className="container mx-auto flex items-center justify-between p-4">
+                <Link
+                    href="/"
+                    className="text-2xl font-bold flex items-center gap-2"
+                >
+                    <Clover className="h-6 w-6 text-green-700" />
+                    <span className="font-thin">
+                        Amigo da{" "}
+                        <span className="font-bold">
+                            Sorte
+                        </span>
+                    </span>
+                </Link>
+
+                <nav className="flex items-center space-x-4">
+                    <Link
+                        href="/app/grupos"
+                        className="text-foreground text-sm flex items-center gap-2"
+                    >
+                        <UsersRound className="h-4 w-4" />
+                        Meus grupos
+                    </Link>
+
+                    <Button asChild variant="outline">
+                        <Link href="/app/grupos/novo">
+                            Novo grupo
+                        </Link>
+                    </Button>
+                </nav>
+            </div>
+        </header>
+    )
+}
